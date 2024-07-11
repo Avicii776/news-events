11388e6f84e0db345bdafa52c329e36ebed81c6c
lycoding <18934808647@163.com>
2024-07-11 17:28:08 +0800

    初始化项目

--- src/main/java/org/lycoding/utils/AliOSSUtils.java
+++ src/main/java/org/lycoding/utils/AliOSSUtils.java
@@ -20,25 +20,11 @@ import java.util.UUID;
 
 @Component
 public class AliOSSUtils {
-    final static private String End_Point = "https://oss-cn-hangzhou.aliyuncs.com";
-    final static private String Access_Key_Id = "LTAI5t6xp7zjCg1x3VBGtK5Y";
-    final static private String Access_Key_Secret = "UUZjAxizBOOrLU3ScjEWiOFnO3OIuQ";
-    final static private String Bucket_Name = "bsuc";
+
+
 
     public static String upload(String fileName, InputStream inputStream) throws IOException {
         String url = "";
-        try {
-
-            //上传文件到 OSS
-            OSS ossClient = new OSSClientBuilder().build(End_Point, Access_Key_Id, Access_Key_Secret);
-            ossClient.putObject(Bucket_Name, fileName, inputStream);
-
-            //文件访问路径
-            url = End_Point.split("//")[0] + "//" + Bucket_Name + "." + End_Point.split("//")[1] + "/" + fileName;
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
 
         return url;// 把上传到oss的路径返回
     }
