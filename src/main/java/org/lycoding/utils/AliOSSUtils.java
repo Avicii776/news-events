e54247c5457f1046b9b4e30f341fd470588a15f0
lycoding <18934808647@163.com>
2024-07-17 13:07:32 +0800

    给老子打回

--- src/main/java/org/lycoding/utils/AliOSSUtils.java
+++ src/main/java/org/lycoding/utils/AliOSSUtils.java
@@ -1,6 +1,8 @@
 package org.lycoding.utils;
 
 
+import com.aliyun.oss.OSS;
+import com.aliyun.oss.OSSClientBuilder;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
@@ -8,11 +10,25 @@ import java.io.InputStream;
 
 @Component
 public class AliOSSUtils {
-
+    final static private String End_Point = "https://oss-cn-hangzhou.aliyuncs.com";
+    final static private String OSS_ACCESS_KEY_ID = System.getenv("OSS_ACCESS_KEY_ID");
+    final static private String OSS_ACCESS_KEY_SECRET = System.getenv("OSS_ACCESS_KEY_SECRET");
+    final static private String Bucket_Name = "bsuc";
 
     public static String upload(String fileName, InputStream inputStream) throws IOException {
         String url = "";
+        try {
 
+            //上传文件到 OSS
+            OSS ossClient = new OSSClientBuilder().build(End_Point, OSS_ACCESS_KEY_ID, OSS_ACCESS_KEY_SECRET);
+            ossClient.putObject(Bucket_Name, fileName, inputStream);
+
+            //文件访问路径
+            url = End_Point.split("//")[0] + "//" + Bucket_Name + "." + End_Point.split("//")[1] + "/" + fileName;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         return url;// 把上传到oss的路径返回
     }
